6d4a7a35a1914f738c2a471c039c56fac237c860
Nicholas <setup@Nicholas-MacBook-Pro.local>
2024-10-24 20:51:09 -0500

    Added Docker and PostgreSQL setup

--- dogprojectbohlen/dogprojectbohlen/settings.py
+++ dogprojectbohlen/dogprojectbohlen/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dogapi',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,14 @@ WSGI_APPLICATION = 'dogprojectbohlen.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'dogdb'),
+        'USER': os.getenv('POSTGRES_USER', 'doguser'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'dogpassword'),
+        'HOST': 'db',  # This matches the service name in docker-compose.yml
+        'PORT': '5432',
     }
 }
 
